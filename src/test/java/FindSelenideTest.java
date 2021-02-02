ed187417de95fa861b1237e6a1a4a03638f66044
vladimir klonin <vklonin@gmail.com>
2021-02-02 21:20:41 +0400

    Initial commit

---
+++ src/test/java/FindSelenideTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+public class FindSelenideTest {
+
+    @Test
+    void shouldFindSelenideGitHub(){
+        // open github
+
+        // insert word selenide in search
+
+        // push enter
+
+        // select a first found project
+
+        // check in subject we have selenide/selenide
+
+        //ARRANGE
+        //ACT
+        //ASSERT
+    }
+
+}
